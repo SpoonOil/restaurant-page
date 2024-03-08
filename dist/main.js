5c79d9699c23c3d8e3b6e7a83bd7b99f671fa9d4
SpoonOil <69192866+SpoonOil@users.noreply.github.com>
2024-03-08 16:00:39 +0000

    initialize webpack and whatnatnot

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hi4')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGtCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoaTQnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
